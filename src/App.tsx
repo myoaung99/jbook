2c097d2e50f75a9443825847916c9927f2e14f2f
MyoMyintAung <myomyintaung064@gmail.com>
2022-12-29 10:25:35 +0630

    added transpiling process with esbuild.transform

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import { useState } from 'react'
-
+import * as esBuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const serviceRef = useRef<any>();
+
+  const startService = async () => {
+    esBuild
+      .initialize({
+        worker: true,
+        wasmURL: "/esbuild.wasm",
+      })
+      .then(() => {
+        //* track the esBuild ref to the rest of the app
+        serviceRef.current = esBuild;
+      });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const clickHandler = async () => {
+    //* transpile and bundle the input
+
+    if (!serviceRef.current) return;
+    const result = await serviceRef.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
+  };
+
   return (
     <div>
-      hi
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+
+      <pre>{code}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
